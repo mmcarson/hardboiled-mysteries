230f4475839c2804c91cb865144223526caa035f
Marissa Carson <mmcarson@coe.edu>
2019-10-21 09:17:56 -0500

    working resolution thing!
    height for menu is off though

--- togglemenu.js
+++ togglemenu.js
@@ -1,7 +1,8 @@
 function toggleMenu() {
     if (document.getElementById("main-nav").style.height == "0px") {
         var numChildren = document.getElementById("main-nav").childNodes.length;
-        document.getElementById("main-nav").style.height = (numChildren * 1.5) + "em";
+        document.getElementById("main-nav").style.height = ((numChildren + 1) * 1.5) + "em";
+        // document.getElementById("main-nav").style.height = "100vh";
         document.getElementById("burger").classList.add("open");
     } else {
         document.getElementById("main-nav").style.height = "0px";
