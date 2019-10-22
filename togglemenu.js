7b29a50d9c6c135a2c2b4e806ab83633c57af6d7
Marissa Carson <mmcarson@coe.edu>
2019-10-22 12:32:26 -0500

    working on background stuff

--- togglemenu.js
+++ togglemenu.js
@@ -1,7 +1,8 @@
 function toggleMenu() {
     if (document.getElementById("main-nav").style.height == "0px") {
         var numChildren = document.getElementById("main-nav").childNodes.length;
-        document.getElementById("main-nav").style.height = ((numChildren + 1) * 1.5) + "em";
+        var heightFactor = window.getComputedStyle(document.body).getPropertyValue('--height-factor');
+        document.getElementById("main-nav").style.height = ((numChildren) * heightFactor) + "em";
         // document.getElementById("main-nav").style.height = "100vh";
         document.getElementById("burger").classList.add("open");
     } else {
