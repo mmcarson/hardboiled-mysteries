c9575c06ba591c946f875644d037aec6ff236153
Marissa Carson <mmcarson@coe.edu>
2020-02-07 13:59:35 -0600

    ready to go except collapse

--- countdown.js
+++ countdown.js
@@ -1,21 +1,19 @@
 const eventDate = Date.parse("03 Jan 2020");
 
-console.log(document)
-
 if (Date.now() <= eventDate) {
     document.getElementById("number-days").innerText = Math.ceil((eventDate - Date.now()) / (1000 * 60 * 60 * 24));
 } else {
     countdown = document.getElementById("countdown")
     countdown.parentNode.removeChild(countdown)
-    ticketButtons = document.getElementsByClassName("ticket-button")
-    console.log(ticketButtons)
-    for (let i = 0; i < ticketButtons.length; i++) {
-        console.log(ticketButtons[i].parentNode)
-        parent = ticketButtons[i].parentNode
-        parent.removeChild(ticketButtons[i])
+    document.querySelectorAll(".ticket-button").forEach(button => {
+        console.log({ button })
+        console.log(button.parentNode)
+        parent = button.parentNode
+        parent.removeChild(button)
             // console.log(parent.innerText).replace(" ", "8")
         if (parent.innerText === "") {
             parent.parentNode.removeChild(parent)
         }
-    }
+    })
+
 }
